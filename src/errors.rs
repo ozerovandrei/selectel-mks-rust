47f651b6dc54fa40242c238fcc9913ee960793e3
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-08-12 13:45:28 +0300

    Implement basic make request logic
    Add MKS::new_request, MKS::do_request, MKS::make_uri methods to prepare
    and make a HTTP request.
    
    Add new error variants into MKSError enum.
    
    Add http and tokio dependencies to Cargo.toml.

--- src/errors.rs
+++ src/errors.rs
@@ -1,18 +1,54 @@
 /// MKS error return type.
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub enum MKSError {
     /// Bad endpoint value.
     EndpointError,
 
     /// Empty token value.
     EmptyTokenError,
+
+    /// HTTP response contains bad status code.
+    HttpError(u16, String),
+
+    /// Failed to perform HTTP request with Hyper.
+    HyperError(hyper::Error),
+
+    /// Error while building a new request.
+    RequestError,
+
+    /// Request timed out.
+    TimeoutError,
+
+    /// Bad URL for a new request.
+    UrlError,
 }
 
 impl std::fmt::Display for MKSError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
+        match &*self {
             MKSError::EndpointError => "failed to parse base endpoint URL".fmt(f),
             MKSError::EmptyTokenError => "token cannot be empty".fmt(f),
+            MKSError::HttpError(status, err) => {
+                format!("bad status code: {}, error body: {}", status, err).fmt(f)
+            }
+            MKSError::HyperError(err) => {
+                format!("failed to make the request due to Hyper error: {}", err).fmt(f)
+            }
+            MKSError::RequestError => "failed to build a new request".fmt(f),
+            MKSError::TimeoutError => "request timed out".fmt(f),
+            MKSError::UrlError => "failed to parse URL for request".fmt(f),
         }
+    }
+}
+
+impl std::convert::From<hyper::Error> for MKSError {
+    fn from(e: hyper::Error) -> Self {
+        MKSError::HyperError(e)
+    }
+}
+
+impl std::convert::From<tokio::time::Elapsed> for MKSError {
+    fn from(_e: tokio::time::Elapsed) -> Self {
+        MKSError::TimeoutError
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 mod errors;
 
 use errors::MKSError;
+use http::{HeaderMap, StatusCode};
 use std::time::Duration;
+use tokio::time::timeout;
 use url::Url;
 
 // Hyper imports.
-use hyper::Client;
+use hyper::body::Buf;
+use hyper::header::{HeaderValue, CONTENT_LENGTH, CONTENT_TYPE, USER_AGENT};
+use hyper::{Client, Method, Request};
 #[cfg(feature = "rustls")]
 type HttpsConnector = hyper_rustls::HttpsConnector<hyper::client::HttpConnector>;
 #[cfg(feature = "rust-native-tls")]
@@ -74,6 +78,94 @@ impl MKS {
     pub fn builder() -> Builder {
         Builder::default()
     }
+
+    // Prepare a new request.
+    fn new_request(
+        &mut self,
+        method: Method,
+        path: &str,
+        body: Option<String>,
+    ) -> Result<Request<hyper::Body>, MKSError> {
+        // Build a final Hyper URI.
+        let uri = self.make_uri(path)?;
+
+        // Prepare a new Hyper request.
+        let mut req = Request::new(hyper::Body::empty());
+        *req.method_mut() = method;
+        *req.uri_mut() = uri.clone();
+
+        // Add user-agent header.
+        req.headers_mut().insert(
+            USER_AGENT,
+            HeaderValue::from_str(self.user_agent.as_str()).map_err(|_| MKSError::RequestError)?,
+        );
+
+        // Add x-auth-token header.
+        req.headers_mut().insert(
+            "x-auth-token",
+            HeaderValue::from_str(self.token.as_str()).map_err(|_| MKSError::RequestError)?,
+        );
+
+        // Add body into the new request if it's provided.
+        if let Some(body) = body {
+            // Add content-length header if body is provided.
+            let len = HeaderValue::from_str(&body.len().to_string())
+                .map_err(|_| MKSError::RequestError)?;
+            req.headers_mut().insert(CONTENT_LENGTH, len);
+
+            // Add content-type header if body is provided.
+            req.headers_mut().insert(
+                CONTENT_TYPE,
+                HeaderValue::from_str("application/json").map_err(|_| MKSError::RequestError)?,
+            );
+
+            *req.body_mut() = hyper::Body::from(body);
+        }
+
+        Ok(req)
+    }
+
+    #[tokio::main]
+    async fn do_request(&self, req: hyper::Request<hyper::Body>) -> Result<Response, MKSError> {
+        let mut headers = HeaderMap::new();
+        let duration = self.timeout.clone();
+        let handle = async {
+            let raw_resp = self.client.request(req).await?;
+
+            headers = raw_resp.headers().clone();
+            let status = raw_resp.status();
+            let body = hyper::body::aggregate(raw_resp).await?.to_bytes();
+            let body = String::from_utf8_lossy(&body);
+
+            Ok::<_, hyper::Error>((body.to_string(), status))
+        };
+
+        let raw_resp = timeout(duration, handle).await??;
+
+        let (body, status) = raw_resp;
+
+        if !status.is_success() {
+            return Err(MKSError::HttpError(status.as_u16(), body.to_string()));
+        }
+
+        Ok(Response {
+            status,
+            headers,
+            body,
+        })
+    }
+
+    fn make_uri(&self, path: &str) -> Result<hyper::Uri, MKSError> {
+        let url = self
+            .base_endpoint
+            .clone()
+            .join(path)
+            .map_err(|_| MKSError::UrlError)?;
+
+        url.as_str()
+            .parse::<hyper::Uri>()
+            .map_err(|_| MKSError::UrlError)
+    }
 }
 
 /// Builder for `MKS`.
@@ -121,6 +213,13 @@ impl Builder {
     }
 }
 
+/// Response represents a result of an HTTP request.
+struct Response {
+    status: StatusCode,
+    headers: HeaderMap,
+    body: String,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -152,21 +251,5 @@ mod tests {
         assert_eq!(mks.token, String::from("token_b"));
         assert_eq!(mks.user_agent, format!("{}/{}", PKG_NAME, PKG_VERSION));
         assert_eq!(mks.timeout, Duration::from_secs(10));
-    }
-
-    #[test]
-    fn new_mks_bad_base_endpoint() {
-        assert_eq!(
-            MKS::new("bad_url", "token_c").err(),
-            Some(errors::MKSError::EndpointError)
-        );
-    }
-
-    #[test]
-    fn new_mks_emtpy_token() {
-        assert_eq!(
-            MKS::new("https://example.org", "").err(),
-            Some(errors::MKSError::EmptyTokenError)
-        );
     }
 }
