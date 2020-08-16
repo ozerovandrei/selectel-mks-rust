c55c8ad08a552969c0017c74a337e148a5f7327d
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-08-16 15:09:25 +0300

    Implement get_cluster method for the Client
    Implement a method to get a cluster.
    
    Add Status enum, Cluster, ClusterRoot and KubernetesOptions structs.
    
    Add an integration test.

---
+++ tests/cluster.rs
@@ -0,0 +1,26 @@
+use std::env;
+
+pub mod common;
+
+#[test]
+fn get_cluster() {
+    if !common::integration_tests_are_enabled() {
+        return;
+    }
+
+    let cluster_id = env::var(common::TEST_CLUSTER_ID).expect(
+        format!(
+            "Failed to read {} environment variable to test get_cluster method",
+            common::TEST_CLUSTER_ID
+        )
+        .as_str(),
+    );
+
+    let client = common::setup();
+    let cluster = client
+        .get_cluster(cluster_id.as_str())
+        .expect("Failed to get a cluster");
+
+    assert_eq!(cluster.id, cluster_id);
+    println!("Cluster: {:?}\n", cluster);
+}
